428ee16693d37effc80523804630dc08670a9422
Yashi6633 <nishathyasintha@gmail.com>
2023-05-17 00:12:06 +0530

    Initiate MVC

---
+++ app/controllers/homeController.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once '../app/core/controller.php';
+// require_once 'announcementController.php';
+
+class homeController extends controller{
+
+    function __construct(){
+        parent::__construct();
+    }
+
+    public function index(){
+        $this->view->render('404errorView');
+    }
+
+    public function home(){
+        $this->loadModel('homeModel');
+        $this->view->users = $this->model->readTable();
+        $this->view->render('homeView');
+    }
+    
+
+    public function login(){
+        $this->view->render('loginView');
+    }
+}
